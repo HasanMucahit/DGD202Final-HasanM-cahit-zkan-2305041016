12fec651a95355abb452ebad2478a6885418ed52
HasanMucahit <ozkanhasanmucahit@gmail.com>
2025-06-17 15:46:36 +0300

    hareket eklendi

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static PlayerInput Instance;
+
+    public float horizontalInput;
+    public float verticalInput;
+    void Awake()
     {
-        
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        print(verticalInput);
     }
 }
--- Assets/Scripts/Player/PlayerMover.cs
+++ Assets/Scripts/Player/PlayerMover.cs
@@ -4,23 +4,26 @@ using UnityEngine.InputSystem;
 public class PlayerMover : MonoBehaviour
 {
     private Rigidbody _rigidbody;
-    
+
     [Header("Movement Parameters")]
     [field: SerializeField] public float MoveSpeed { get; private set; }
     [SerializeField] private float _turnSpeed;
-    
+
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
     }
-
+    private void Update()
+    {
+        Move(new Vector2(PlayerInput.Instance.horizontalInput, PlayerInput.Instance.verticalInput));
+    }
     public void Move(Vector2 direction)
     {
         float moveX = direction.x;
         float moveZ = direction.y;
 
         transform.Rotate(Vector3.up, moveX * _turnSpeed * Time.deltaTime, Space.World);
-        
+
         _rigidbody.linearVelocity = transform.forward * (moveZ * MoveSpeed);
     }
 }
